2593c2662d7112707fe6e13ea2850ee5e571f21f
sohan <sohanzid@gmail.com>
2022-07-06 13:02:47 +0600

    chng home

--- app/src/main/java/com/example/cardiacrecorder/Home.java
+++ app/src/main/java/com/example/cardiacrecorder/Home.java
@@ -2,13 +2,42 @@ package com.example.cardiacrecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+    ImageView addRecord,showHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        addRecord=findViewById(R.id.addRecord);
+        showHistory=findViewById(R.id.showHistory);
+
+
+        /* making image clickable to jump to record adding */
+        addRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, addRecord.class);
+                startActivity(intent);
+            }
+        });
+
+        /* making image clickable to jump to show bp recorded */
+        showHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, showHistory.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
