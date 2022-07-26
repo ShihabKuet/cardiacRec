bb87dea3d339e25d0c0c9e21fd8b6be53d976e3a
sohan <sohanzid@gmail.com>
2022-07-27 02:01:52 +0600

    add activity show

--- app/src/main/java/com/example/cardiacrecorder/showHistory.java
+++ app/src/main/java/com/example/cardiacrecorder/showHistory.java
@@ -1,6 +1,9 @@
 package com.example.cardiacrecorder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,34 +12,52 @@ import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class showHistory extends AppCompatActivity {
-    Button b;
-    FirebaseAuth mAuth;
-    FirebaseUser mUser;
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    recviewAdapter recviewadapter;
+    ArrayList<bp> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_history);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-        mUser = mAuth.getCurrentUser();
-        b=findViewById(R.id.signoutbtn);
+        recyclerView = findViewById(R.id.recview);
+        database = FirebaseDatabase.getInstance().getReference("bp_record_pass");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        b.setOnClickListener(new View.OnClickListener() {
+        list = new ArrayList<>();
+        recviewadapter = new recviewAdapter(this,list);
+        recyclerView.setAdapter(recviewadapter);
+
+        database.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                signOutUser();
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    bp b = dataSnapshot.getValue(bp.class);
+
+                    list.add(b);
+                }
+                recviewadapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
 
-    private void signOutUser() {
-        Intent intent = new Intent(showHistory.this, Login.class);
-        startActivity(intent);
-        finish();
-    }
+
 }
